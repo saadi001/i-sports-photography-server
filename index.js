6939d8974712a6ad38476e7b9c7a8bf634b3f224
Sheikh sadi <you@example.com>
2022-11-10 19:39:18 +0600

    review by service added

--- index.js
+++ index.js
@@ -38,6 +38,31 @@ async function run(){
                const result = await reviewCollection.insertOne(review);
                res.send(result);
           })
+
+          app.get('/reviews', async(req, res)=>{
+               let query = {}
+               if(req.query.email){
+                    query= {
+                         email: req.query.email
+                    }
+               }
+
+               const cursor = reviewCollection.find(query)
+               const reviews = await cursor.toArray()
+               res.send(reviews)
+          })
+
+          app.get('/reviews', async(req, res)=>{
+               let query = {}
+               if(req.query.service){
+                    query= {
+                         service: req.query.service
+                    }
+               }
+               const cursor = reviewCollection.find(query)
+               const reviewById = await cursor.toArray()
+               res.send(reviewById)
+          })
      }
      finally{
           
